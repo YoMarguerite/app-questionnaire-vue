d26de2ec4af766d301602c1593f05d73d8c70d33
Yoann MARGUERITE <yoann.marguerite@epsi.fr>
2019-11-19 14:12:47 +0100

    fix bug affichage résultat

--- src/views/Resultat.vue
+++ src/views/Resultat.vue
@@ -1,22 +1,24 @@
 <template>
   <div>
-    <b-card v-if="all == null" class="mt-3" header="Mes Résultats">
+
+    <b-card v-if="all === undefined" class="mt-3" header="Mes Résultats">
       <div v-for="(questions,index) in questionnaires" :key="index">
-        <b-button @click="visible[index-1] = !visible[index-1]" variant="primary">Afficher test du {{questions.date}}</b-button>
-        <b-collapse v-model="visible[index-1]">
+        <b-button @click="inverseVisibility(-index-1)" variant="primary" class="mb-1">Afficher test du {{questions.date}}</b-button>
+        <b-collapse v-model="visible[-index-1]">
           <Result  :date="questions.date" :questions="questions"></Result>
         </b-collapse>
       </div>
     </b-card>
-    <b-card v-if="all == true" class="mt-3" header="Tout les résultats">
-      
+
+    <b-card v-if="all === true" class="mt-3" header="Tout les résultats">
       <div v-for="(questions,index) in questionnaires" :key="index">
-        <b-button @click="visible[index-1] = !visible[index-1]" variant="primary">Afficher test du {{questions.date}} par {{questions.user.firstname}} {{questions.user.lastname}} ({{questions.user.company}})}}</b-button>
-        <b-collapse v-model="visible[index-1]">
+        <b-button @click="inverseVisibility(-index-1)" variant="primary" class="mb-1">Afficher test du {{questions.date}} par {{questions.user.firstname}} {{questions.user.lastname}} ({{questions.user.company}})</b-button>
+        <b-collapse v-model="visible[-index-1]">
           <Result  :date="questions.date" :questions="questions"></Result>
         </b-collapse>
       </div>
     </b-card>
+
   </div>
 </template>
 
@@ -44,19 +46,19 @@
       }
       try{
          this.questionnaires = JSON.parse(localStorage.getItem("results"))
-         if(this.all === null){
+         if(this.all === undefined){
           this.questionnaires = this.questionnaires.filter((q) => {
-            //eslint-disable-next-line
-            console.log(q.user.firstname)
             return (q.user.firstname===this.user.firstname)&&(q.user.lastname===this.user.lastname)&&(q.user.company===this.user.company)
           })
          }
       }catch(e){
         this.questionnaires = []
       }
-      this.questionnaires.map(() => {
-        this.visible.push(false)
-      })
+    },
+    methods:{
+      inverseVisibility(index){
+        this.visible[index] = !this.visible[index]
+      }
     }
   }
 </script>
