2a5fb19f9a828afc152cf9fd627e9a7a2f0dc9b2
Yoann MARGUERITE <yoann.marguerite@epsi.fr>
2019-11-19 13:07:18 +0100

    déconnexion

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,19 @@
   <div id="app">
     <b-navbar variant="primary" type="dark">
       <b-navbar-brand tag="h1" class="mb-0">
-        <router-link to="/">
-          <b-button variant="primary">AppQuestion</b-button>
-        </router-link>
+        <b-button variant="primary" @click="signOut">AppQuestion</b-button>
       </b-navbar-brand>
       <b-navbar-nav class="ml-auto">
         <b-dropdown v-if="user != null" :text="user.firstname+' '+user.lastname+' ('+user.company+')'" variant="primary">
           <b-dropdown-item @click="$router.push('/questionnaire')">Commencer le test</b-dropdown-item>
-          <b-dropdown-item>Mes résultats</b-dropdown-item>
-          <b-dropdown-item v-if="(user.firstname == 'admin')&&(user.lastname == 'admin')&&(user.company == 'admin')">Tous les Résultats</b-dropdown-item>
+          <b-dropdown-item @click="$router.push('/my_results')">Mes résultats</b-dropdown-item>
+          <b-dropdown-item v-if="(user.firstname == 'admin')&&(user.lastname == 'admin')&&(user.company == 'admin')" @click="$router.push('/all_results')">Tous les Résultats</b-dropdown-item>
           <b-dropdown-divider></b-dropdown-divider>
           <b-dropdown-item @click="signOut">Déconnexion</b-dropdown-item>
         </b-dropdown>
       </b-navbar-nav>
     </b-navbar>
-    <router-view class="container" @setUser="setUser"/>
+    <router-view :key="$route.path" class="container" @setUser="setUser"/>
   </div>
 </template>
 
